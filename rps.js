4909132e61db9fe7a751cc4d7de05b8ee43c95d7
amyburgh <air.myburgh@gmail.com>
2022-09-20 12:08:25 +0200

    Fix js file function naming

--- rps.js
+++ rps.js
@@ -1,57 +1,78 @@
 // Rock Paper Scissors Game
 
-const score = { user: 0, pc: 0 };
-const choices = ["Rock", "Paper", "Scissors"];
+const CHOICES = ["Rock", "Paper", "Scissors"];
 
-let repeat = true;
-while (repeat) {
-    const pcChoice = () => choices[Math.floor(Math.random() * 3)];
-
-    const userInput = prompt(`
+const randomChoice = () => CHOICES[Math.floor(Math.random() * 3)];
+const userInput = () => prompt(`
             Make your choice:
             1. Rock
             2. Paper
             3. Scissors
         `);
 
-    // Clean User Input
-    function cleanUserInput(userInput) {
-        if (parseInt(userInput) == 1 || userInput.toUpperCase() == "ROCK" || userInput.toUpperCase() == "R")
-            return "Rock";
-        else if (parseInt(userInput) == 2 || userInput.toUpperCase() == "PAPER" || userInput.toUpperCase() == "P")
-            return "Paper";
-        else if (parseInt(userInput) == 3 || userInput.toUpperCase() == "SCISSORS" || userInput.toUpperCase() == "S")
-            return "Scissors";
+// Clean User Input
+function cleanUserInput(userInput) {
+    if (parseInt(userInput) == 1 || userInput.toUpperCase() == "ROCK" || userInput.toUpperCase() == "R")
+        return "Rock";
+    else if (parseInt(userInput) == 2 || userInput.toUpperCase() == "PAPER" || userInput.toUpperCase() == "P")
+        return "Paper";
+    else if (parseInt(userInput) == 3 || userInput.toUpperCase() == "SCISSORS" || userInput.toUpperCase() == "S")
+        return "Scissors";
+}
+
+function playRPS(user, pc, score) {
+    console.log("User: ", user);
+    console.log("PC: ", pc);
+    let result = "";
+
+    if (user == pc) {
+        result = 'DRAW'
+        return `It's a ${result}!`;
+    }
+    else if (user == "Rock" && pc == "Scissors"
+        || user == "Paper" && pc == "Rock" ||
+        user == "Scissors" && pc == "Paper") {
+        result = "WIN";
+        score.user++;
+    } else {
+        result = "LOOSE";
+        score.pc++;
     }
 
-    function playRPS(user, pc) {
-        console.log("User: ", user);
-        console.log("PC: ", pc);
-        let result = "";
+    return result == "WIN" ? `You ${result}! ${user} beats ${pc}` :
+        `You ${result}! ${user} beats ${pc}`;
+}
 
-        if (user == pc) {
-            result = 'DRAW'
-            return `It's a ${result}!`;
-        }
-        else if (user == "Rock" && pc == "Scissors"
-            || user == "Paper" && pc == "Rock" ||
-            user == "Scissors" && pc == "Paper") {
-            result = "WIN";
-            score.user++;
-        } else {
-            result = "LOOSE";
-            score.pc++;
-        }
+function play() {
+    let score = { user: 0, pc: 0 };
 
-        return result == "WIN" ? `You ${result}! ${user} beats ${pc}` :
-            `You ${result}! ${user} beats ${pc}`;
+    console.log("=== Best of 5 ===");
+
+    while (score.user < 3 && score.pc < 3) {
+        const pcChoice = randomChoice();
+        let userChoice;
+        do {
+            userChoice = cleanUserInput(userInput());
+
+            if (!userChoice) {
+                console.log("Error: user input")
+            }
+        } while (userChoice == undefined);
+
+        const string = playRPS(userChoice, pcChoice, score);
+
+        console.log(string);
+        console.log(score);
     }
 
-    const string = playRPS(cleanUserInput(userInput), pcChoice());
+    console.log(score.user > score.pc ? "YOU BEAT THE PC!" : "PC BEAT YOU!");
 
-    console.log(string);
-    console.log(score);
     repeat = parseInt(prompt(`Pay Again?
         1: No
         2: Yes`)) - 1;
+    if (repeat == true) {
+        play();
+    }
 }
+
+play();
